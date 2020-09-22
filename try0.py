64decc90c33c41489349c5ca3f69a4ac214a5b3c
Lewous <lmnsodv@gmail.com>
2020-09-22 17:30:17 +0800

    try using multiprocessing

--- try0.py
+++ try0.py
@@ -4,9 +4,15 @@
 
 # from ekmapTK import line_count as lc
 # from ekmapTK import filter
-from ekmapTK import data_read as dr
+from ekmapTK import TOTAL_LINE, data_read as dr
+from ekmapTK import beauty_time as bt
 
-# from tqdm import tqdm
+from numpy import sqrt
+from numpy import pi
+
+from time import time
+from multiprocessing import Pool
+from tqdm import tqdm
 
 wt = 50      # width per part taken
 itl = 1/wt   # interval  is 1/50
@@ -14,10 +20,35 @@ itl = 1/wt   # interval  is 1/50
 
 # import dataset, REFIT in this case
 # and reformat
-f = 'REFIT/CLEAN_House1.csv'
-data2 = dr(f)
-# total_line = data2[-1]
-# print(total_line)
+# f = 'REFIT/CLEAN_House1.csv'
+# data2 = dr(f)
+# # total_line = data2[-1]
+# # print(total_line)
 
-t = '='*6
-print(t + 'done' + t)
+# t = '='*6
+# print(t + 'done' + t)
+
+
+# try multiprocessing
+def do1(x):
+    x = x+515000000
+    y = x**pi
+    return int(''.join(list(str(x))))
+
+
+if __name__ == "__main__":
+    tic = time()
+    TOTAL_LINE = 5000000
+    with tqdm(total = TOTAL_LINE, leave = False, ascii = True, 
+            bar_format = "Loading House1...{l_bar}{bar}|{n_fmt}/{total_fmt}") as pybar:
+
+        for x in range(TOTAL_LINE):
+            x = x+515000000
+            y = x**pi
+            z = y**pi
+            z = int(''.join(list(str(int(z)))))
+            pybar.update(1)
+
+    toc = time()
+    print(bt(toc-tic))
+
