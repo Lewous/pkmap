c27a57251df9d9efd2a1f76b95317e3e324a931b
Lewous <lmnsodv@gmail.com>
2020-09-22 23:32:28 +0800

    create multiprocess with pool.map

--- ekmapTK.py
+++ ekmapTK.py
@@ -5,13 +5,17 @@
 
 # from pandas.core.indexes.datetimes import date_range
 # from ekmapTK0 import TOTAL_LINE
+from unittest import result
 from numpy import fabs
 from numpy import log
 from numpy import nan, isnan
 from numpy import divmod
+from numpy import linspace
+from numpy.lib.function_base import iterable
 
-# from pandas import DataFrame
+from pandas import DataFrame
 from pandas import read_csv
+from multiprocessing import Pool
 
 from time import time
 from tqdm import tqdm
@@ -21,6 +25,8 @@ import re
 
 TOTAL_LINE = 6960002
 FILE_PATH = 'REFIT/CLEAN_House1.csv'
+val0 = {}
+data0 = DataFrame([])
 
 
 def line_count(filepath):
@@ -140,8 +146,20 @@ def beauty_time(time):
 
     return str_time
 
+def do2(arg2):
+    x2, val, data0, c2 = arg2
+    # print(x2)
+    for k in data0.loc[x2[0]:x2[1]-1, c2].iterrows():
+        # combinate new row as a key of a dict
+        nw = ''.join(k[1].astype('int8').astype('str'))
+        if isnan(val[nw]):
+            val[nw] = 1
+        else:
+            val[nw] += 1
+    
+    return val
 
-def data_read(filepath = "", ):
+def data_read(filepath = "", PN = 16):
     """
     ready data to plot
 
@@ -160,19 +178,22 @@ def data_read(filepath = "", ):
 
     global TOTAL_LINE
     global FILE_PATH
+    global data0
+
     if filepath == "":
         filepath = FILE_PATH
     filename = filepath.split('/')[-1].split('.')[:-1][0]
 
-    with tqdm(leave = False, 
-            bar_format = "reading " + filename + " ...") as pybar:
-        data0 = read_csv(filepath)
+    if data0.empty:
+        with tqdm(leave = False, 
+                bar_format = "reading " + filename + " ...") as pybar:
+            data0 = read_csv(filepath)
 
-    TOTAL_LINE = len(data0.index)
-    # appliance quantity
-    appQ = len(data0.columns) - 4
-    print("find `" + str(appQ) + "' appliance with `" + 
-            str(TOTAL_LINE) + "' lines")
+        TOTAL_LINE = len(data0.index)
+        # appliance quantity
+        appQ = len(data0.columns) - 4
+        print("find `" + str(appQ) + "' appliance with `" + 
+                str(TOTAL_LINE) + "' lines")
 
     # data0.rename(columns = {'Appliance' + str(k+1): 'app' + str(k+1) 
     #     for k in range(appQ)}) 
@@ -184,6 +205,7 @@ def data_read(filepath = "", ):
     '''
     # filter here 
     # add later as it's not necessary
+
     '''
     
     # transfer to on/off value
@@ -200,6 +222,9 @@ def data_read(filepath = "", ):
     '''
     # create a dict templet
     # and then fill in the container
+    '''
+    val0 is the template incase lose of keys()
+    '''
     val0 = {}
     for k in GC(4):
         for j in GC(4):
@@ -212,35 +237,45 @@ def data_read(filepath = "", ):
     # fill in statics
     # c2: choose 8 app to analysis (app3 don't looks good)
     c2 = re.findall('Appliance[1,2,4-9]', ''.join(data0.columns))
-    tic = time() 
-    with tqdm(total = TOTAL_LINE, leave = False, ascii = True, 
-            bar_format = "counting...{l_bar}{bar}|{n_fmt}/{total_fmt}|") as pybar:
-    # for k in tqdm(data0.loc[:, c2].iterrows()):
-        for k in data0.loc[0:500000, c2].iterrows():
-            # combinate new row as a key of a dict
-            nw = ''.join(k[1].astype('int8').astype('str'))
-            # True --> 1 --> '1'
+    # c2 is a list of string 
+    tic = time()
+    # PN = 16     # number of process
+    x1 = linspace(0, TOTAL_LINE/100, num = PN + 1, dtype = 'int')
+    x2 = ((x1[k], x1[k+1]) for k in range(PN))
+    # x2 is a generator
+    print(x1)
+    result = list(range(PN))
+    with tqdm(leave = False, bar_format = "Pooling ...") as pybar:
+    # with tqdm(total = TOTAL_LINE * appQ, leave = False, ascii = True, 
+    #         bar_format = "Counting ...{l_bar}{bar}|{n_fmt}/{total_fmt}") as pybar:
+        with Pool(16) as pool:
+            # for k in range(PN):
+            #     x = next(x2)
+            #     print(x)
+            result = pool.map(do2,  ((k, val0.copy(), data0.copy(), c2.copy()) for k in x2) )
+            pool.close()
+            pool.join()
 
-            if isnan(val0[nw]):
-                val0[nw] = 1
-            elif val0[nw] > 0:
-                val0[nw] += 1
-            else:
-                Warning(({'nw':nw, 'Val0':val0[nw]}, 'not mentioned'))
-            pybar.update(1)
-
-    # caculating time cost
     toc = time()
     print('finish counting in ' + beauty_time(toc-tic))
-    [print(k) for k in val0.items()]
+
+    val = {k: sum([result[t][k] for t in range(len(result))]) for k in result[0].keys()}
+    # print(val.items())
+
+    # [print(k) for k in val.items()]
 
 
-    return 0
+    return PN, x1[1], beauty_time(toc-tic)
 
 
 if __name__ == "__main__":
     f = 'REFIT/CLEAN_House1.csv'
-    data2 = data_read(f)
+    with open('aa.txt', 'w') as f:
+        f.write('PN \t width \t costs\n')
+        for k in range(16):
+            data2 = data_read(f, PN = k+1)
+            print(data2)
+            f.write(str(data2[0]) + '\t' + str(data2[1]) + '\t' + data2[2])
 
     t = '='*6
     print(t + 'done' + t)
