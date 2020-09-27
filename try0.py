0fc8c9b9a5b0dd399e69535f6bb5a604d3f1c0a0
Lewous <lmnsodv@gmail.com>
2020-09-27 20:03:48 +0800

    draw heatmap with specified rectangle highlight

--- try0.py
+++ try0.py
@@ -13,7 +13,8 @@ from ekmapTK import do_plot
 # from numpy import int8
 # from numpy import linspace
 
-
+# import matplotlib.pyplot as plt
+import matplotlib.patches as pat
 # from time import time
 # from multiprocessing import Pool, Process, Manager
 # from multiprocessing import Queue
@@ -29,14 +30,21 @@ if __name__ == "__main__":
     data2, appQ = read_REFIT(file_path, slice=slice)
     # file_path = 'REFIT/EKMap_House' +  str(house_number) + '.csv'
     # data2, appQ = read_EKfile(file_path)
+    
+    highL = []  # rectangle to high light
+    for wid, x in zip((2, 4, 4, 4, 2), (0, 6, 14, 22, 30)):
+        highL.append(pat.Rectangle((x, 2), wid, 4, 
+                                fill=False, edgecolor='c', lw=6))
+        highL.append(pat.Rectangle((x, 10), wid, 4, 
+                                fill=False, edgecolor='c', lw=6))
+    # do_plot(data2, appQ, do_show=True, pats=highL, 
+    #         title='1in1_app8 is on')
 
-    # do_plot(data2, appQ, title = 'dfsfs')
 
     k = 0
     for data3 in data2:
         k += 1
-        titl = str(k) + r'in' + str(slice)
-        do_plot(data3, appQ, title = titl, do_show=False, 
+        titl = str(k) + r'in' + str(slice) + '_app3 is on and app8 is off'
+        do_plot(data3, appQ, title = titl, do_show=False, pats=highL.copy(), 
                 fig_type=(r'_' + titl+'.png', r'_' + titl + '.eps', ), )
-
 
