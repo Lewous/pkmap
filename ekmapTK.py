b91830127e401684f508108c47cbd943ba608b64
Lewous <lmnsodv@gmail.com>
2020-11-27 13:38:54 +0800

    new feature: marks on the margin of single

--- ekmapTK.py
+++ ekmapTK.py
@@ -7,10 +7,12 @@ from numpy import sum
 from numpy import fabs, ceil, sqrt
 from numpy import log
 from numpy import nan, isnan
-from numpy import divmod
+from numpy import divmod, mod
 from numpy import linspace, arange
 from numpy import full
 from numpy import save, load
+from numpy import array
+from numpy import pi
 
 from pandas import DataFrame
 from pandas import read_csv
@@ -27,6 +29,7 @@ import matplotlib.patches as pat
 # import seaborn as sn
 from copy import copy
 import os
+
 
 TOTAL_LINE = 6960002
 FILE_PATH = './REFIT/CLEAN_House1.csv'
@@ -134,6 +137,81 @@ def KM(a, b):
 
     return DataFrame(full([2**a, 2**b], nan),
                      index=GC(a), columns=GC(b))
+
+
+def GM(n):
+    """
+    create expond for margin of WKMap
+    """
+
+    n = int(abs(n))
+    n2 = 2**n
+    if n == 0:
+        return ()
+    elif n == 1:
+        return (((1,1.995), ),)
+    else:
+        # new margin
+        new_m = tuple([(2*k+1)/2**(n-1) for k in range(2**(n-1))])
+        new_m2 = tuple([(new_m[2*k], new_m[2*k+1]) for k in range(2**(n-2))])
+        return tuple([k for k in GM(n-1)] + [new_m2])
+
+
+def GMI(n):
+    """
+    create margin index of each four sides
+    nx: quantity of variables in x-axis
+    ny: quantity of variables in y-axis
+    """
+    n = int(abs(n))
+    if n < 4:
+        raise ValueError('n = ' + str(n) + ' < 4 is not accepted!')
+    ny = int(n/2)
+    nx = n - ny
+    # nx, ny = int(abs(nx)), int(abs(ny))
+    n_R = int(ny/2)   # marks at right
+    n_L = ny - n_R      # marks at left
+    n_T = int(nx/2)   # marks at top
+    n_B = nx - n_T      # marks at bottom
+    n = nx + ny         # total variables
+
+    my = GM(ny)
+    tk = True      # pointer, to n_L if True, to n_R if Flase
+    nA_L, nA_R = n_L, n_R   # marker of A(nA_L)
+    R, L = {}, {}           # output container
+    An = 1          # this shares both x and y
+    for m in my:
+        m2 = tuple([tuple((array(mt)*2**ny-1)/2) for mt in m])
+        if tk:
+            L['A'+str(An)] = (nA_L, m2)
+            tk ^= 1
+            An += 1
+            nA_L -= 1
+        else:
+            R['A'+str(An)] = (nA_R, m2)
+            tk ^= 1
+            An += 1
+            nA_R -= 1
+    
+    mx = GM(nx)
+    tk = True      # pointer, to n_L if True, to n_R if Flase
+    nA_B, nA_T = n_B, n_T   # marker of A(nA_L)
+    B, T = {}, {}           # output container
+    for m in mx:
+        m2 = tuple([tuple((array(mt)*2**nx-1)/2) for mt in m])
+        # print((m, m2))
+        if tk:
+            B['A'+str(An)] = (nA_B, m2)
+            tk ^= 1
+            An += 1
+            nA_B -= 1
+        else:
+            T['A'+str(An)] = (nA_T, m2)
+            tk ^= 1
+            An += 1
+            nA_T -= 1
+    
+    return ((n_L, n_R, n_B, n_T), L, R, B, T)
 
 
 def beauty_time(time):
@@ -392,6 +470,196 @@ def new_order(ahead=(), appQ=9):
     return tuple(order2)
 
 
+def do_plot2(data3, cmap='inferno', fig_types=(), do_show=True,
+                   titles="", pats=[]):
+    """
+    plot WKMap with markers around
+    data3: a dict 
+
+    """
+    global file_name
+    print(f'{file_name=}')
+    # fill in data
+    appQ = len(tuple(data3.keys())[0])  # total number of appliance
+    nx = int(appQ / 2)
+    ny = appQ - nx
+
+    n_M, *M = GMI(appQ)
+    n_L, n_R, n_B, n_T = n_M
+    wdd = {         # n_of_variables: (wdx, wdy, magnify_of_lxy)
+        7: (0.8, 1.2),
+        9: (0.8, 1.3, 0.6), 
+    }
+    wdx = 0.6
+    wdy = 1
+    wd1 = 0.3
+    wd2 = 0
+    mg = 0.4
+    lx = mg*2**nx
+    ly = mg*2**ny
+    # if mod(appQ, 2):
+    fig = plt.figure(figsize=((n_T+n_B)*wdy+ly, (n_L+n_R)*wdx+lx))
+    # fig = plt.figure(figsize=(ly, lx))
+    gs = fig.add_gridspec(3, 3,  
+                width_ratios=(n_L*wdx, lx, n_R*wdx), 
+                height_ratios=(n_T*wdy, ly, n_B*wdy),
+                left=0, right=1, bottom=0, top=1,
+                wspace=0.05, hspace=0.05)
+    # else:
+    #     fig = plt.figure(figsize=(8, 8))
+    #     gs = fig.add_gridspec(3, 3,  
+    #                 width_ratios=(n_L*wd, 6, n_R*wd), 
+    #                 height_ratios=(n_T*wd, 6, n_B*wd),
+    #                 left=0.1, right=0.9, bottom=0.1, top=0.9,
+    #                 wspace=0.05, hspace=0.05)
+
+    ax = fig.add_subplot(gs[1,1])
+    # ====== `ekmap' is the contant of a subplot ======
+    ekmap = KM(nx, ny)      # preparing a container
+    ekback = KM(nx, ny)     # backgroud color
+    ek = 1
+    vmax = log(sum(tuple(data3.values())))
+
+    for _ind in ekmap.index:
+        for _col in ekmap.columns:
+            d = data3[_ind + _col]
+            if d:
+                # d > 0
+                ekmap.loc[_ind, _col] = log(d)/ek
+            else:
+            #     # d == 0
+                ekback.loc[_ind, _col] = 0.04
+    
+    ax.imshow(ekback, cmap='Blues',vmin=0, vmax=1)
+    ax.imshow(ekmap, alpha = 1, cmap=cmap, vmin=0, vmax=vmax)
+    ax.set_yticks(arange(2**nx))
+    ax.set_xticks(arange(2**ny))
+    ax.set_yticklabels(ekmap.index.values, fontfamily='monospace')
+    ax.set_xticklabels(ekmap.columns.values,
+                       fontfamily='monospace', rotation=45)
+    print(f'{nx=}'+', '+f'{ny=}')
+    # ax.spines['top'].set_visible(False)
+    # ax.spines['right'].set_visible(False)
+    # ax.spines['bottom'].set_visible(False)
+    # ax.spines['left'].set_visible(False)
+    # ax.axis('off')
+
+    for S, pl, xy in zip(M, (gs[1, 0], gs[1,2], gs[2,1], gs[0,1]), ('L','R','B','T')):
+        # S = L, R, B, T
+        if xy in ('B', 'T'):
+            ax_S = fig.add_subplot(pl, sharex=ax)
+        elif xy in ('L', 'R'):
+            ax_S = fig.add_subplot(pl, sharey=ax)
+        else:
+            ax_S = fig.add_subplot(pl)
+        print(f'{S=}')
+        cf1 = wd1
+        cf2 = 0.1
+        for margin in S.items():
+            mag_n = margin[0]
+            # like `A1'
+            mag_v = margin[1]
+            # mag_v like `(2, ((3.5, 7.48),))'
+            # or `(1, ((0.5, 2.5), (4.5, 6.5)))'
+            ofst = mag_v[0]     # offset, a number start from 1
+            # `mag_v[1]' like `((3.5, 7.48),)'
+            # or `((0.5, 2.5), (4.5, 6.5))'
+            for ind in mag_v[1]:
+                m_st = ind[0]           # start
+                m_itl = ind[1] - ind[0]     # interval
+                m_tx = m_st + m_itl/2       # place to text, parallel to the axis
+                m_ty = cf1*ofst - cf2       # vartical to the axis
+                if   xy in ('L', ):
+                    ax_S.add_patch(plt.Rectangle(
+                        (cf1, m_st), 0-cf1-m_ty, m_itl, fill=False))
+                    ax_S.text(0-m_ty, m_tx, mag_n, alpha=1, 
+                        ha='center', va='center', backgroundcolor='w', 
+                        family='monospace', size='large')
+                elif xy in ('R', ):
+                    ax_S.add_patch(plt.Rectangle(
+                        (0-cf1, m_st), cf1+m_ty, m_itl, fill=False))
+                    ax_S.text(m_ty, m_tx, mag_n, alpha=1, 
+                        ha='center', va='center', backgroundcolor='w', 
+                        family='monospace', size='large')
+                elif xy in ('B', ):
+                    ax_S.add_patch(plt.Rectangle(
+                        (m_st, cf1), m_itl, 0-cf1-m_ty, fill=False))
+                    ax_S.text(m_tx, 0-m_ty, mag_n, alpha=1, 
+                        ha='center', va='center', backgroundcolor='w', 
+                        family='monospace', size='large')
+                elif xy in ('T', ):
+                    ax_S.add_patch(plt.Rectangle(
+                        (m_st, 0-cf1), m_itl, cf1+m_ty, fill=False))
+                    ax_S.text(m_tx, m_ty, mag_n, alpha=1, 
+                        ha='center', va='center', backgroundcolor='w',
+                        family='monospace', size='large')
+        
+        if   xy in ('L', ):
+            ax_S.set_xlim(left=0-n_L*wd1+wd2, right=0)
+        elif xy in ('R', ):
+            ax_S.set_xlim(left=0, right=n_R*wd1-wd2)
+        elif xy in ('B', ):
+            ax_S.set_ylim(bottom=0-n_B*wd1+wd2, top=0)
+        elif xy in ('T',):
+            ax_S.set_ylim(bottom=0, top=n_T*wd1-wd2)
+        # ax_S.axis('off')
+
+    # ax_B = fig.add_subplot(gs[2,1], sharex=ax)
+    # ax_B.add_patch(plt.Rectangle((1.5, 1), 1.99, -1.2, fill=False))
+    # ax_B.text(2.5, -0.2, r'A3', 
+    #     ha='center', va='center', backgroundcolor='w',
+    #     family='monospace', size='xx-large')
+    # ax_B.set_ylim(bottom=-0.3, top=0)
+    # ax_B.axis('off')
+
+    # ax_L = fig.add_subplot(gs[1,0], sharey=ax)
+    # ax_L.add_patch(plt.Rectangle((1, 1.5), -1.2, 1.99, fill=False))
+    # ax_L.text(-0.2, 2.5, r'A1', alpha=1, 
+    #     ha='center', va='center', backgroundcolor='w',
+    #     family='monospace', size='xx-large')
+    # ax_L.set_xlim(left=-0.3, right=0)
+    # ax_L.axis('off')
+
+    # ax_R = fig.add_subplot(gs[1,2], sharey=ax)
+    # ax_R.add_patch(plt.Rectangle((-1, 0.5), 1.2, 2, fill=False))
+    # ax_R.text(0.2, 1.5, r'A2', alpha=1, 
+    #     ha='center', va='center', backgroundcolor='w', 
+    #     family='monospace', size='xx-large')
+    # ax_R.set_xlim(left=0, right=0.3)
+    # ax_R.axis('off')
+
+
+    title = copy(titles)
+    if title:
+        # `title' has been specified
+        ax.set_title(title, size=24)
+        # see in https://stackoverflow.com/questions/42406233/
+        pass
+
+    if pats:
+        # `pats' is not empty, do aditional draw
+        for pat in pats:
+            ax.add_patch(copy(pat))
+            # see in https://stackoverflow.com/questions/47554753
+    # fig.tight_layout()
+
+    for fig_type in fig_types:
+        plt.pause(1e-13)
+        # see in https://stackoverflow.com/questions/62084819/
+        plt.savefig('./figs/EKMap' +
+                    file_name[5:] + 
+                    # str(time())[-5:] + 
+                    fig_type, 
+                    bbox_inches='tight')
+
+    if do_show:
+        plt.show()
+    else:
+        plt.close(fig)
+
+    return fig
+
+
 def do_plot_single(data3, cmap='inferno', fig_types=(), do_show=True,
                    titles="", pats=[]):
     """
@@ -405,7 +673,7 @@ def do_plot_single(data3, cmap='inferno', fig_types=(), do_show=True,
     nx = int(appQ / 2)
     ny = int(appQ - nx)
 
-    fig1, ax = plt.subplots(1, 1, figsize=(8, 5))
+    fig, ax = plt.subplots(1, 1, figsize=(8, 5))
 
     # ====== `ekmap' is the contant of a subplot ======
     ekmap = KM(nx, ny)      # preparing a container
@@ -430,10 +698,10 @@ def do_plot_single(data3, cmap='inferno', fig_types=(), do_show=True,
     ax.imshow(ekmap, alpha = 1, cmap=cmap, vmin=0, vmax=vmax)
     ax.set_yticks(arange(2**nx))
     ax.set_xticks(arange(2**ny))
-    ax.set_yticklabels(ekmap.index.values, fontfamily='monospace')
+    ax.set_yticklabels(ekmap.index.values, 
+        family='monospace', size='xx-large')
     ax.set_xticklabels(ekmap.columns.values,
-                       fontfamily='monospace', rotation=45)
-    
+        fontfamily='monospace', size='xx-large', rotation=45)
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
@@ -451,21 +719,21 @@ def do_plot_single(data3, cmap='inferno', fig_types=(), do_show=True,
         for pat in pats:
             ax.add_patch(copy(pat))
             # see in https://stackoverflow.com/questions/47554753
-    fig1.tight_layout()
+    # fig.tight_layout()
 
     for fig_type in fig_types:
         plt.pause(1e-13)
         # see in https://stackoverflow.com/questions/62084819/
         plt.savefig('./figs/EKMap' +
-                    file_name[5:] + fig_type, 
+                    file_name[5:] +str(time.time())[-5:] + fig_type, 
                     bbox_inches='tight')
 
     if do_show:
         plt.show()
     else:
-        plt.close(fig1)
+        plt.close(fig)
 
-    return 0
+    return fig
 
 
 def do_plot_multi(data3, cmap='inferno', fig_types=(), do_show=True,
@@ -496,9 +764,9 @@ def do_plot_multi(data3, cmap='inferno', fig_types=(), do_show=True,
     num_row = int(ceil(sqrt(n_slice)))
     num_col = int(ceil(n_slice / num_row))
 
-    # fig1, axes= plt.subplots(num_row, num_col, figsize=(15, 8))
+    # fig, axes= plt.subplots(num_row, num_col, figsize=(15, 8))
     fsize = (int(num_row * 2**(ny-nx)*3), num_col*4)
-    fig1, axes = plt.subplots(
+    fig, axes = plt.subplots(
         num_col, num_row, figsize=fsize)
     print(f'{fsize=}')
 
@@ -550,7 +818,7 @@ def do_plot_multi(data3, cmap='inferno', fig_types=(), do_show=True,
         ax = axes[ind2[0], ind2[1]]
         ax.axis('off')
 
-    fig1.tight_layout()
+    fig.tight_layout()
 
     for fig_type in fig_types:
         plt.pause(1e-13)
@@ -561,9 +829,9 @@ def do_plot_multi(data3, cmap='inferno', fig_types=(), do_show=True,
     if do_show:
         plt.show()
     else:
-        plt.close(fig1)
+        plt.close(fig)
 
-    return 0
+    return fig
 
 
 def do_plot(data2, ahead=(), cmap='inferno', fig_types=(), do_show=True,
@@ -699,7 +967,7 @@ def slice_REFIT(args):
 if __name__ == "__main__":
     files = findall('(CLEAN_House[0-9]+).csv', '='.join(listdir('REFIT')))
     # files = ['CLEAN_House8']
-    print(f'{files=}')
+    # print(f'{files=}')
 
     # this code will exhibit my novel assessment
     # for house_number, slice in ((5, 4, ), ):
@@ -709,6 +977,8 @@ if __name__ == "__main__":
     #     do_plot(data2, (0,), titles=tuple(str(k+1) + r'in' + str(slice) for k in range(slice)),
     #             do_show=True, fig_types=('in' + str(slice) + '.png', ),
     #             )
-
+    x = GMI(7)
+    for ind in x:
+        print((ind))
     t = '='*6
     print(t + ' finished ' + t)
