1d751f23a408c17d9e19a4c057c20ef5793ca97a
zj.lewous <lmnsodv@gmail.com>
2020-12-01 07:20:59 +0800

    using `FancyBbox'

--- try0.py
+++ try0.py
@@ -45,6 +45,13 @@ AD = {'0000': 0,
       '1000': 1282001, 
       }
 
+# pre-set pats data: x_data, y_data, width, hight
+pat_data = {
+    '378': ((0.5, 16.5), (1.5, 9.5), 14, 4),
+    '-3-7-8': ((-2.5, 13.5, 29.5), (-2.5, 5.5, 13.5), 4, 4),
+    '-3-4-7-8': ((-1.5, 14.5, 30.5), (-1.5, 6.5, 14.5), 2, 2),
+    '489': ((0.5,8.5,16.5,24.5), (0.5,4.5,8.5,12.5),6,2)
+}
 
 def GAD(n):
     """
@@ -76,8 +83,8 @@ def do1(house_number, slice):
     # highL.append(pat.Rectangle((15.5, 5.5), 1, 10,
     #                            fill=False, edgecolor='c', lw=3))
 
-    for wid, x in zip((2, 4, 4, 4, 2), (0, 6, 14, 22, 30)):
-        # highL.append(pat.Rectangle((x, 2), wid, 4,
+    for x, y in zip((0.5, 0.5, 16.5, 16.5), (1.5, 9.5, 1.5, 9.5)):
+        # highL.append(pat.Rectangle((x, y), 6, 4,
         #                         fill=False, edgecolor='c', lw=6))
         # highL.append(pat.Rectangle((x, 10), wid, 4,
         #                         fill=False, edgecolor='c', lw=6))
@@ -91,11 +98,9 @@ def do1(house_number, slice):
         # fig_types=(r'_' + titl+'.png', r'_' + titl + '.eps', ), )
         # fig_types=(r'_' + titl+'.png', ), )
 
-    do_plot(data2, (0,), titles=tuple(str(k+1) + r'in' + str(slice) for k in range(slice)),
+    return do_plot(data2, (0,), titles=tuple(str(k+1) + r'in' + str(slice) for k in range(slice)),
             do_show=True, pats=highL, fig_types=('in' + str(slice) + '.eps', ),
             )
-
-    return None
 
 
 def do2(house_number, slice, n_app):
@@ -112,25 +117,71 @@ def do2(house_number, slice, n_app):
         pool.close()
         pool.join()
 
-    print('-'*20)
-    pass
-
-    return None
+    return print('-'*20)
 
 
-def do3(house_number):
+def do3(house_number, titles='', **kwargs):
     """
     do basic single plot
     """
     file_path = 'REFIT/CLEAN_House' + str(house_number) + '.csv'
     data2 = read_REFIT(file_path)
     
-    do_plot2(data2, titles='House'+str(house_number), do_show=False, 
-            fig_types=('_.eps', ))
+    do12(data2, '-3-7-8', fig_types=('_1.eps','_1.png',),
+        do_show=False, 
+        # titles='House'+str(house_number), 
+        **kwargs)
+    do12(data2, '-3-4-7-8', fig_types=('_1.eps','_1.png',),
+        do_show=False, 
+        # titles='House'+str(house_number), 
+        **kwargs)
+    return do12(data2, 378, ('_2.eps','_2.png',),
+                do_show=False, 
+                # titles='House'+str(house_number), 
+                **kwargs)
+    
+
+def do12(data, p2hs, cols = ('c', 'b', 'k'), **kwargs):
+    '''
+    plot 9 varibales' WKMap with pats
+    data: data to plot
+    p2hs: int, str, iterable (3 max) are accepted
+
+    return:
+    '''
+
+    if isinstance(p2hs, (str, int)):
+        p2hs = (str(p2hs),)
+    elif isinstance(p2hs, (list, tuple)):
+        p2hs = tuple([str(x) for x in p2hs])
+    else:
+        p2hs = ()       # empty tuple
+
+    highL=[]
+    ofst = 0.6      # offset
+    for p2h, col, ofst in zip(p2hs, 
+                             cols, 
+                             (0.2, 0.4, 0.6)):
+        if not p2h:
+            # is empty or None or ...
+            continue
+
+        if p2h in pat_data.keys():
+            xd, yd, wid, hit = pat_data[p2h]
+        else:
+            xd, yd = (0,), (0,)
+            wid, hit = 4, 4
+        for x, y in ((u,v) for u in xd for v in yd):
+            highL.append(pat.FancyBboxPatch(
+                        (x+ofst, y+ofst), wid-ofst*2, hit-ofst*2,
+                        fill=False, color=col, lw=6))
+
+    return do_plot2(data, pats=highL, **kwargs)
+
 
 if __name__ == "__main__":
 
-    house_number = 8
+    house_number = 17
     slice = 9
     n_app = 3
     # house_number = (1, 2, 3, 4, 5, 
@@ -140,14 +191,16 @@ if __name__ == "__main__":
     # do1(house_number, slice)
     # do2(house_number, slice, n_app)
 
-    for k in (1, 2, 3, 4, 5, 
-              6, 7, 8, 9, 10,
-              11, 12, 13, 15, 16, 
-              17, 18, 19, 20, 21, ):
-        do3(k)
+    # for k in (1, 2, 3, 4, 5, 
+    #           6, 7, 8, 9, 10,
+    #           11, 12, 13, 15, 16, 
+    #           17, 18, 19, 20, 21, ):
+    #     do3(k)
         
-    # do3(house_number)
+    do3(house_number)
     # AD = GAD(9)
     # print(AD)
-    do_plot2(AD, fig_types=('-.eps',), do_show=True)
-    print(f'{sum(AD.values())}')
+    p2h =''    # pats to hightlight
+    # do12(AD, p2h, )
+    # do_plot2(AD, fig_types=('.png',), pats=highL, do_show=True)
+    # # print(f'{sum(AD.values())}')
